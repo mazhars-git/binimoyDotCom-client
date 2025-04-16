b734212e0cc12d730de11784a09a86a968338ee1
Juboraj Sujon <mdsujon72@gmail.com>
2025-04-16 12:25:00 +0600

    Sujon: Create dashboard admin layout and pages

---
+++ src/app/(WithDashboardLayout)/dashboard/(AdminLayout)/admin/listings/page.tsx
@@ -0,0 +1,15 @@
+const AdminLintingManagePage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">Admin Route: Linting Manage Page</h1>
+      <p className="mt-2 text-lg">
+        This is the admin route for managing linting.
+      </p>
+      <p className="mt-2 text-lg">
+        You can add your linting management logic here.
+      </p>
+    </div>
+  );
+};
+
+export default AdminLintingManagePage;
