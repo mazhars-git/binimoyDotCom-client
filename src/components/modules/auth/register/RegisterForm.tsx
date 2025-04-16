31f9d34aa77177154f8b852a2035e3125a626466
Juboraj Sujon <mdsujon72@gmail.com>
2025-04-16 12:36:22 +0600

    Sujon: added register page and component

---
+++ src/components/modules/auth/register/RegisterForm.tsx
@@ -0,0 +1,153 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Link from "next/link";
+import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
+import { registrationSchema } from "./registerValidation";
+import { registerUser } from "@/services/AuthService";
+import { toast } from "sonner";
+import { useRouter } from "next/navigation";
+import { useUser } from "@/context/UserContext";
+
+const RegisterForm = () => {
+  const form = useForm({
+    resolver: zodResolver(registrationSchema),
+  });
+  const router = useRouter();
+
+  const {
+    formState: { isSubmitting },
+  } = form;
+
+  const password = form.watch("password");
+  const passwordConfirm = form.watch("passwordConfirm");
+
+  const { setIsLoading } = useUser();
+
+  const onSubmit: SubmitHandler<FieldValues> = async (data) => {
+    try {
+      const res = await registerUser(data);
+      setIsLoading(true);
+      if (res?.success) {
+        toast.success(res?.message);
+        router.push("/");
+      } else {
+        toast.error(res?.message);
+      }
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+  return (
+    <div className="border-2 border-gray-300 rounded-xl flex-grow max-w-md w-full p-5 space-y-5">
+      <div className="flex items-center space-x-4 ">
+        <span className="text-3xl font-bold">AD</span>
+        <div>
+          <h1 className="text-xl font-semibold">Register</h1>
+          <p className="font-extralight text-sm text-gray-600">
+            Join us today and start your journey!
+          </p>
+        </div>
+      </div>
+      <Form {...form}>
+        <form className="space-y-3" onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Phone Number</FormLabel>
+                <FormControl>
+                  <Input type="text" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} value={field.value || ""} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="passwordConfirm"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirm Password</FormLabel>
+                <FormControl>
+                  <Input type="password" {...field} value={field.value || ""} />
+                </FormControl>
+                {passwordConfirm && password !== passwordConfirm ? (
+                  <FormMessage> Password does not match </FormMessage>
+                ) : (
+                  <FormMessage />
+                )}
+              </FormItem>
+            )}
+          />
+
+          <Button
+            disabled={Boolean(passwordConfirm && password !== passwordConfirm)}
+            type="submit"
+            className="mt-5 w-full">
+            {isSubmitting ? "Registering..." : "Register"}
+          </Button>
+        </form>
+      </Form>
+      <p className="text-sm text-gray-600 text-center my-3">
+        Already have an account ?
+        <Link href="/login" className="text-primary ml-2">
+          Login
+        </Link>
+      </p>
+    </div>
+  );
+};
+
+export default RegisterForm;
