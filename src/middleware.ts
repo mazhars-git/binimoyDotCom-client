9b2263441df9d5c6ed204e7c6af4093af10a8486
Juboraj Sujon <mdsujon72@gmail.com>
2025-04-16 12:30:33 +0600

    Sujon: added middleware

---
+++ src/middleware.ts
@@ -0,0 +1,60 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getCurrentUser } from "./services/AuthService";
+
+type Role = keyof typeof roleBasedPrivateRoutes;
+
+const authRoutes = ["/login", "/register"];
+
+const roleBasedPrivateRoutes = {
+  user: [/^\/dashboard(?!\/admin)/, /^\/cart/, /^\/messages/],
+  admin: [/^\/dashboard\/admin/],
+};
+
+export const middleware = async (request: NextRequest) => {
+  const { pathname } = request.nextUrl;
+
+  const userInfo = await getCurrentUser();
+
+  if (!userInfo) {
+    if (authRoutes.includes(pathname)) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(
+        new URL(
+          `${process.env.NEXT_PUBLIC_CLIENT_BASE_API}/login?redirectPath=${pathname}`,
+          request.url
+        )
+      );
+    }
+  }
+
+  if (userInfo?.role && roleBasedPrivateRoutes[userInfo?.role as Role]) {
+    const routes = roleBasedPrivateRoutes[userInfo?.role as Role];
+    if (routes.some((route) => pathname.match(route))) {
+      return NextResponse.next();
+    }
+  }
+
+  return NextResponse.redirect(new URL("/", request.url));
+};
+
+export const config = {
+  matcher: [
+    "/cart",
+    "/dashboard",
+    "/dashboard/:page",
+    "/dashboard/profile",
+    "/dashboard/listing",
+    "/dashboard/listing/:page",
+    "/dashboard/sales-history",
+    "/dashboard/purchase-history",
+    "/messages",
+    "/messages/:page",
+    "/dashboard/admin",
+    "/dashboard/admin/:page",
+    "/dashboard/admin/user-management",
+    "/dashboard/admin/user-management/:page",
+    "/dashboard/admin/listings",
+    "/dashboard/admin/listings/:page",
+  ],
+};
