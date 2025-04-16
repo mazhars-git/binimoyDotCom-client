6cc93bb04b02b586fc4e9aa7d180b70d174ede57
Juboraj Sujon <mdsujon72@gmail.com>
2025-04-16 12:22:42 +0600

    Sujon: Create message page

---
+++ src/app/(WithCommonLayout)/messages/page.tsx
@@ -0,0 +1,11 @@
+const CartPage = () => {
+  return (
+    <div>
+      <h1>Messages Page</h1>
+      <p>This is the messages page.</p>
+      <p>Here you can view and manage your messages.</p>
+    </div>
+  );
+};
+
+export default CartPage;
