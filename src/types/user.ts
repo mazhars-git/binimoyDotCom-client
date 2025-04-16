1b620b6f016f024296bd13d9e1ea36605003cbe4
Juboraj Sujon <mdsujon72@gmail.com>
2025-04-16 12:38:16 +0600

    Sujon: added user types

---
+++ src/types/user.ts
@@ -0,0 +1,20 @@
+export interface IUser {
+  email: string;
+  role: "user" | "admin";
+  iat?: number;
+  exp?: number;
+}
+
+export interface IUserDeatails {
+  _id: string;
+  name: string;
+  email: string;
+  phoneNumber: string;
+  photo: string;
+  role: "user" | "admin";
+  status: "active" | "inactive";
+  isBan: boolean;
+  createdAt: string;
+  updatedAt: string;
+  __v: number;
+}
