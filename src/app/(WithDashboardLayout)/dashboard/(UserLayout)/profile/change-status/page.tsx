87e49ff2a571b478b4e4935b17f966d59785336a
Juboraj Sujon <mdsujon72@gmail.com>
2025-04-22 15:04:58 +0600

    Sujon: Added change component

---
+++ src/app/(WithDashboardLayout)/dashboard/(UserLayout)/profile/change-status/page.tsx
@@ -0,0 +1,22 @@
+import ChangeStatusForm from "@/components/modules/profile/ChangeStatusForm";
+import { getCurrentUserDetails } from "@/services/AuthService";
+import { Metadata } from "next";
+
+//metadata
+export const metadata: Metadata = {
+  title: "Account Setting | AdolBodol",
+  description:
+    "View and manage your personal information, account settings, and activity on AdolBodol — your trusted platform for buying and selling second-hand items.",
+};
+
+const AdminProfile = async () => {
+  const currentUser = await getCurrentUserDetails();
+
+  return (
+    <div className="flex justify-center items-center">
+      <ChangeStatusForm user={currentUser?.data} />
+    </div>
+  );
+};
+
+export default AdminProfile;
